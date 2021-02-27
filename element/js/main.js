8fd86e3b17d508db3ba1928db6e6884ccf08e7ca
AcediaMiz <50464861+AcediaMiz@users.noreply.github.com>
2021-02-28 01:18:22 +0500

    element

---
+++ element/js/main.js
@@ -0,0 +1,8 @@
+document.querySelector(".menu-btn").addEventListener("click", () => {
+  document.querySelector(".menu").classList.toggle("active");
+  document.querySelector(".menu-btn").classList.toggle("active");
+});
+document.querySelector(".close").addEventListener("click", () => {
+  document.querySelector(".menu").classList.toggle("active");
+  document.querySelector(".menu-btn").classList.toggle("active");
+});
