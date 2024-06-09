23437db2e97f51d9bd80e468755bc059845af98e
AcediaMiz <50464861+AcediaMiz@users.noreply.github.com>
2024-06-10 01:12:14 +0500

    ikki

--- Longread/main.js
+++ Longread/main.js
@@ -428,3 +428,13 @@ survey.applyTheme({
   },
   headerView: "basic",
 })
+let prevScrollpos = window.pageYOffset
+window.onscroll = function () {
+  var currentScrollPos = window.pageYOffset
+  if (prevScrollpos > currentScrollPos) {
+    document.getElementById("navbar").style.top = "0"
+  } else {
+    document.getElementById("navbar").style.top = "-500px"
+  }
+  prevScrollpos = currentScrollPos
+}
